4381ca092faec2efd853ec9b424a6fa916f3729e
Runyasak Chaengnaimuang <world.jadi@gmail.com>
2021-02-09 00:11:59 +0700

    feat: implement class and protocol

--- Protocols Demo/main.swift
+++ Protocols Demo/main.swift
@@ -1,11 +1,59 @@
-//
-//  main.swift
-//  Protocols Demo
-//
-//  Created by Runyasak Chaengnaimuang on 8/2/2564 BE.
-//
+protocol CanFly {
+    func fly()
+}
 
-import Foundation
 
-print("Hello, World!")
+class Bird {
 
+    var isFemale = true
+    
+    func lazyEgg() {
+        if isFemale {
+            print("The bird make a new bird in a shell.")
+        }
+    }
+}
+
+class Eagle: Bird, CanFly {
+    func fly() {
+        print("The eagle flaps its wings and lifts off into the sky.")
+    }
+    
+
+    func soar() {
+        print("The eagle glides in the air using air currents.")
+    }
+}
+
+class Penguin: Bird {
+    
+    func swim() {
+        print("The penguin paddles through the water.")
+    }
+}
+
+struct FlyingMuseum {
+    func flyingDemo(flyingObject: CanFly) {
+        flyingObject.fly()
+    }
+}
+
+class Airplane: CanFly {
+    func fly() {
+        print("The airplane uses its engine to lift off into the air.")
+    }
+}
+
+let myEagle = Eagle()
+myEagle.fly()
+myEagle.lazyEgg()
+myEagle.soar()
+
+let myPenguin = Penguin()
+myPenguin.lazyEgg()
+myPenguin.swim()
+
+let myPlane = Airplane()
+
+let museum = FlyingMuseum()
+museum.flyingDemo(flyingObject: myPlane)
